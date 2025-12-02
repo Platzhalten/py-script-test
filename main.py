d7271fc32874e5e3b44fe10a11b4f07a599b139a
Platzhalten <github@platzmacher.org>
2025-12-02 10:19:41 +0000

    removed som things from main.py

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 from pyscript import display
 import sys
-import FreeSimpleGUI as sg
-
-layout = [[sg.T("ICH FRESS NEN BESEN")]]
-
-w = sg.Window(title="GEIL", layout=layout)
-
-w.read()
-
-while True:
-  e, v = w.read()
-
-  if e is None:
-    w.close()
-    break
